e6a57ed8a30229ab4341182a12f534b9c0019b62
renaldyoliveira4-dotcom <email-da-conta-correta@gmail.com>
2026-06-10 14:40:41 -0300

    Fix Meta Pixel ID 2186186862219304

--- app/layout.tsx
+++ app/layout.tsx
@@ -114,7 +114,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '859453667211129');
+fbq('init', '2186186862219304');
 fbq('track', 'PageView');`
         }</Script>
 
